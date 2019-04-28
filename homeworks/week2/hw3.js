5af0432e05f0d3ab0302b7ea25394b148bedfa82
pvt5r486 <pvt5r486@gmail.com>
2019-04-28 12:26:28 +0800

    'week2_homework'

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,11 @@
 function reverse(str) {
-  return str;
+  let tempStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    tempStr += str[i];
+  }
+  return tempStr;
 }
 
-reverse('hello');
+console.log(reverse('yoyoyo'));
+console.log(reverse('1abc2'));
+console.log(reverse('1,2,3,2,1'));
